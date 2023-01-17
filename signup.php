480bd27eec9e3cbff71c2b5058af88d343a4c33d
Brayan <destroyergarzon@gmail.com>
2023-01-16 22:55:29 -0500

    login

--- signup.php
+++ signup.php
@@ -1,7 +1,26 @@
 <?php 
-require 'database.php';
+    require 'database.php';
+    $message = ''; /*variable de mensaje*/ 
 
+    if (!empty($_POST['email']) && !empty($_POST['password'])){
+        $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':email',$_POST['email']);
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT); /*cifrar contraseñas*/ 
+        $stmt->bindParam(':password',$password);
+
+
+        if ($stmt->execute()){
+            $message = 'SUCCESSFULLY CREATED NEW USER';
+        } else {
+            $message = "SORRY THERE MUST HAVE BEEN AN ISSUE CREATING YOUR PASSWORD";
+        }
+
+
+    }
 ?>
+
 
 
 <!DOCTYPE html>
@@ -28,19 +47,21 @@ require 'database.php';
 </head>
 <body>
     
-
-    
+    <?php if(!empty($message)):?>
+        <p><?php $message ?></p>
+    <?php endif; ?>
     <div class="card">
         <div class="card2">
             
 
 
             <form action="index.php" method="post">
-                <h1>Admin-Login</h1>
+                <h1>Sing-Up</h1>
                 <input class="input" type="text" name="email" placeholder="Enter your Email" required autocomplete="none">
                 <br>
 
-                <input class="input" type="password" placeholder="Enter your password" required>
+                <input class="input" type="password" placeholder="Enter your password" name="password" required>
+                
                 <br>
                 <div class="button">
                     <input class="input_button" type="submit" value="Enviar">
